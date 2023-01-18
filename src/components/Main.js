4250a2404a429cbd488768b2a002cb5cd9371473
Julian Caicedo <juccaicedoac@unal.edu.co>
2023-01-18 12:20:33 -0500

    create html tags

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <main>
+            <h2>Main content</h2>
+        </main>
+    );
+};
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
