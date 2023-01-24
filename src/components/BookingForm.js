b060abc4111b4818fe072ace49f3d4f11ccdc01d
Julian Caicedo <juccaicedoac@unal.edu.co>
2023-01-24 17:35:48 -0500

    add useReducer

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,20 +1,11 @@
 import { useState } from "react";
 
-const BookingForm = () => {
+const BookingForm = (props) => {
 
     const [date, setDate] = useState(new Date());
-    const [selectedValue, setSelectedValue] = useState("18:00");
+    const [selectedValue, setSelectedValue] = useState(props.availableTimes.times[0]);
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState("birthday");
-    const [availableTimes, setAvailableTimes] = useState([
-        "17:00",
-        "18:00",
-        "19:00",
-        "20:00",
-        "21:00",
-        "22:00",
-    ]);
-
 
     const handleSubmit = (e) => { 
         e.preventDefault(); 
@@ -27,20 +18,20 @@ const BookingForm = () => {
             <input type="date" id="res-date" name="res-date" value={date} required onChange={(e) => {setDate(e.target.value)}}/>
             <label htmlFor="res-time">Choose time</label>
             <select id="res-time" name="res-time" value={selectedValue} required onChange={(e) => {setSelectedValue(e.target.value)}}>
-                {availableTimes.map((time,index) => {
+                {props.availableTimes.times.map((time,index) => {
                     return (
                         <option key={index} value={time}>{time}</option>
                     )
                 })}
             </select>
             <label htmlFor="guest">Number of guest</label>
-            <input type="number" id="guest" name="guest" value={guests} placeholder="1" min="1" max="10" required onClick={(e) => {setGuests(e.target.value)}}/>
+            <input type="number" id="guest" name="guest" value={guests} placeholder="1" min="1" max="10" required onChange={(e) => {setGuests(e.target.value)}}/>
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" name="occasion" value={occasion} required onChange={(e) => {setOccasion(e.target.value)}}>
                 <option value="birthday">Birthday</option>
                 <option value="anniversary">Anniversary</option>
             </select>
-            <input type="submit" value="Make you reservation"/>
+            <button type="submit" value="Make you reservation" onClick={()=> {props.dispatch(props.availableTimes)}}>Make you reservation</button>
         </form>
     )
 }
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,8 +1,29 @@
 import BookingForm from "./BookingForm"
+import { useReducer } from "react";
+
+const initializeTimes = () => {
+    return (
+        {times: [
+            "17:00",
+            "18:00",
+            "19:00",
+            "20:00",
+            "21:00",
+            "22:00",
+        ]}
+    );
+}
+
+const updateTimes = (state, action) => {
+    return state;
+}
 
 const BookingPage = () => {
+
+    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+
     return (
-        <BookingForm/>
+        <BookingForm availableTimes={availableTimes} dispatch={dispatch}/>
     )
 }
 
