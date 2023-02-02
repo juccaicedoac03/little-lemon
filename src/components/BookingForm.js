e5881b0be8a708b7c0bdced896d2aa8c93493c8a
Julian Caicedo <juccaicedoac@unal.edu.co>
2023-02-02 11:48:59 -0500

    add error validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -7,9 +7,13 @@ const BookingForm = (props) => {
     const [date, setDate] = useState( formatDate(new Date()) );
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState("general");
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate();
 
     const validateForm = () => {
+
+        const formErrors = {};
+
         let validDate = true;
         if ( new Date(date.replace(/-/g,'/')) < new Date(formatDate(new Date()).replace(/-/g,'/')) ) {
             validDate = false;
@@ -18,12 +22,14 @@ const BookingForm = (props) => {
         let validGuests = true;
         if ( guests<1 || guests > 15) {
             validGuests = false;
-        }
+        };
 
         let validReservations = false;
         if (props.options.reservationList.length > 0) {
             validReservations = true;
-        }
+        } else {
+            formErrors["reservations"] = "Select a table";
+        };
 
         return (!validDate || !validGuests || !validReservations)
     };
@@ -40,6 +46,15 @@ const BookingForm = (props) => {
         setDate(e.target.value);
         props.dispatch({type: "UPDATE_TIMES", date: e.target.value});
         props.dispatch({type: "UPDATE_AVAILABLE_TIMES", time: props.options.selectedTime, reservations: getReservations(props.options.selectedTables)});
+        if ( new Date(e.target.value.replace(/-/g,'/')) < new Date(formatDate(new Date()).replace(/-/g,'/')) ) {
+            let state = {...errors};
+            state["date"] = "Date must be greater or equal than today";
+            setErrors(state);
+        } else {
+            let state = {...errors};
+            state["date"] = "";
+            setErrors(state);
+        };
         e.target.blur();
     };
 
@@ -51,12 +66,22 @@ const BookingForm = (props) => {
     const handleGuestsChange = (e) => {
         setGuests(e.target.value);
         props.dispatch({type:"UPDATE_SELECTED_GUESTS" , guests: e.target.value});
+        if ( e.target.value<1 || e.target.value > 15) {
+            let state = {...errors};
+            state["guests"] = "Guests must be between 1 and 15";
+            setErrors(state);
+        } else {
+            let state = {...errors};
+            state["guests"] = "";
+            setErrors(state);
+        }
+        ;
     };
-
     return (
         <form onSubmit={handleSubmit} style={{display: "grid", maxWidth: "200px", gap: "20px"}}>
             <label htmlFor="res-date">Choose date</label>
             <input type="date" id="res-date" name="res-date" value={date} required onChange={handleDateChange}/>
+            {(errors.date && errors.date.length>0) && <div className="error">{errors.date}</div>}
             <label htmlFor="res-time">Choose time</label>
             <select id="res-time" name="res-time" value={props.options.selectedTime} required onChange={(e) => {props.dispatch({type: "UPDATE_SELECTED_TIME", time: e.target.value})}}>
                 {props.options.availableTimes.map((time,index) => {
@@ -67,6 +92,7 @@ const BookingForm = (props) => {
             </select>
             <label htmlFor="guest">Number of guest</label>
             <input type="number" id="guest" name="guest" value={guests} placeholder="1" min="1" max="10" required onChange={handleGuestsChange}/>
+            {(errors.guests && errors.guests.length>0) && <div className="error">{errors.guests}</div>}
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" name="occasion" value={occasion} required onChange={handleOcassionChange}>
                 <option value="general">General</option>
